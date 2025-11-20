a823c296be15c28c77c11701211d76af76ba71ff
SolumCrabro <solumcrabro@gmail.com>
2025-11-20 23:43:49 +0300

    add button and style

--- app/src/main/java/com/example/kotlinlesson/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlesson/MainActivity.kt
@@ -14,10 +14,45 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+
+
+        binding.imageView.setImageResource(R.drawable.avatar_300)
+        binding.imageView.tag = R.drawable.avatar_300
+
+
+        binding.button.setOnClickListener {
+
+            if (binding.imageView.tag == R.drawable.avatar_300) {
+
+                binding.imageView.setImageResource(R.drawable.avatar2_300)
+                binding.imageView.tag = R.drawable.avatar2_300
+            } else {
+
+                binding.imageView.setImageResource(R.drawable.avatar_300)
+                binding.imageView.tag = R.drawable.avatar_300
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+          /* binding.cardView1.setOnClickListener {
+               binding.imageView.setImageResource(R.drawable.avatar2_300)*/
+
+              /*if (imageView2.tag == R.drawable.avatar_300)
+                  imageView2.setImageResource(R.drawable.avatar2_300)
+               else
+                   imageView2.setImageResource(R.drawable.avatar_300)*/
+           }
+
+
+
+
+
+
     }
-}
